1fdb251383d69a1fb386ac7c454140215c16f7d5
LPandorf <leticiapandorf@gmail.com>
2022-08-20 23:25:58 -0300

    refe
    freferfr

--- projeto4-parrotscardgame/script.js
+++ projeto4-parrotscardgame/script.js
@@ -1,117 +1,148 @@
-(function(){
-    var matches=0;
-    var images=[];
-    var flippedCards=[];
-    var modalGameOver = document.querySelector("#gameover");
-    var imgMatchSign = document.querySelector("#imgMatchSign");
+let qntDeCartas;
+let baralho=[];
+let tipo=["1","2","3","4","5","6","7"];
+let cartaUm, cartaDois;
+let idCronometro;
+let tempo=0;
+let acertos=0;
+let movimentos=0;
 
-    for(var i = 1; i < 15; i++){
-        var img = {
-            src: "img/"+ i +".jpg",
-            id: i%7
-        };
-        images.push(img);
-    }   
 
-    startGame();
+// aleatoriedade
+function random() {
+    return Math.random() - 0.5;
+}
 
-    function startGame(){
+//cronometro
+function cronometro(){
+    document.querySelector(".relogio").classList.add("mostrar-relogio");
+    tempo++;
+    document.querySelector(".relogio").innerHTML = tempo;
+}
 
-        flippedCards = [];
-
-        images = randomSort(images);
-
-        var frontFaces = document.getElementsByClassName("front");
-
-        for( var i = 0; i<14; i++){
-            
-            var card = document.querySelector("#card"+i);
-            card.style.left = i % 7 === 0 ? 5 +"px" : i % 7*165+5+"px";
-            card.style.top = i < 7 ? 5 + "px": 250 + "px";
-
-            card.addEventListener("click",flipcard,false);
-            frontFaces[i].style.background="url('"+ images[i].src +"')";
-            frontFaces[i].setAttribute("id", images[i].id);
-            
-        }
-
-        modalGameOver.style.zIndex=-2;
-        modalGameOver.removeEventListener("click",startGame,false);
-
+//perguntar e validar quantidade de cartas da partida
+function invalido(){
+    if(qtdDeCartas>=4 && qtdDeCartas<=14 && qntDeCartas%2==0){
+        return false;
+    }else{
+        return true;
     }
+}
+function perguntar(){
+    while(invalido()){
+        qntDeCartas=Number(prompt("Quer jogar com quantas cartas?"));
+    }
+    //função pra começar o jogo (começar) ----------------ok
+    começar(); 
+}
 
-    function randomSort(oldArray){
-        var newArray = [];
+//começar o jogo e distribuir as cartas
+function começar(){
+    for(let i=0; i<qntDeCartas/2; i++){
+        let carta=passaro[i];
+        baralho.push(carta);
+        baralho.push(carta);
+    }
+    baralho.sort(random);
+    //função para mostrar as cartas (mostrar) --------------------ok
+    mostrar()  
+}
 
-        while(newArray.length !== oldArray.length){
-            var i = Math.floor(Math.random()*oldArray.length);
+//mostrar as cartas e assim iniciar de fato o jogo
+function mostrar(){
+    let tabuleiro=document.querySelector(".tabuleiro");
+    for(let i=0; i<baralho.length; i++){
+        //chama a função que desvira a carta (desvira) ---------------ok
+        let cartinha= `
+            <li class="carta virada" onClick="desvira(this)">
+                <div class='frente face'>
+                    <img src='img/back.png'>
+                </div>
+                <div class='costas face'>
+                    <img src='img/${baralho[i]}.jpg'>
+                </div>
+            </li>
+        `;
+        tabuleiro.innerHTML+= cartinha;
+    }
+    //chama função que desvira todas as cartas depois de 2600 segundos (desviraTudo)
+    setTimeout(desviraTudo, 2600);
+}
 
-            if(newArray.indexOf(oldArray[i])<0){
-                newArray.push(oldArray[i]);
+//desvira a carta
+function desvira(carta){
+    if(idCronometro=== undefined){
+        idCronometro= setInterval(cronometro, 2600);
+    }
+    if(carta.classList.contains("virada")){
+        return;
+    }
+    if(cartaUm!=undefined && cartaDois!=undefined){
+        return;
+    }
+    carta.classList.add("virada");
+    movimentos++;
+    if(cartaUm==undefined){
+        cartaUm=carta;
+    }else{
+        if(cartaDois==undefined){
+            cartaDois=carta;
+            if(cartaUm.innerHTML==cartaDois.innerHTML){
+                //chama função que deixa as cartas com as costas para frente (fixa)
+                fixa();
+                acertos+=2;
+                //chama função que checa se todas as cartas ja foram encontradas (confereSeTerminou)
+                confereSeTerminou();
+            }else{
+                //chama função que vira a carta (vira)
+                setTimeout(vira, 2600);
             }
         }
-
-        return newArray;
     }
+}
 
-    function flipcard(){
-        if(flippedCards.length < 2){    
-            var faces = this.getElementsByClassName("face");
-
-            if(faces[0].classList.length>2){
-                return;
-            }
-
-            faces[0].classList.toggle("flipped");
-            faces[1].classList.toggle("flipped");
-
-            flippedCards.push(this);
-
-            if(flippedCards.length===2){
-                if(flippedCards[0].childNodes[3].id=== flippedCards[1].flippedCards[3].id){
-                    flippedCards[0].childNodes[1].classList.toggle("match");
-                    flippedCards[0].childNodes[3].classList.toggle("match");
-                    flippedCards[1].childNodes[1].classList.toggle("match");
-                    flippedCards[1].childNodes[3].classList.toggle("match");
-
-                    matchCardSing();
-
-                    matches++;
-
-                    flippedCards= [];
-
-                    if(matches===7){
-                        gameOver();
-                    }
-
-                }
-            }
-        } else{
-            flippedCards[0].childNodes[1].classList.toggle("flipped");
-            flippedCards[0].childNodes[3].classList.toggle("flipped");
-            flippedCards[1].childNodes[1].classList.toggle("flipped");
-            flippedCards[1].childNodes[3].classList.toggle("flipped");
-
-            flippedCards=[];
-        }
+//desvira todas as cartas depois de 2600 segundos
+function desviraTudo(){
+    const cartas=document.querySelectorAll(".virada");
+    for(let i=0; i<cartas.length; i++){
+        cartas[i].classList.remove("virada");
     }
+}
 
-    
+//deixa as cartas fixas depois de acertar o par, retirando a carta específica da variável antes de virar dnv
+function fixa(){
+    cartaUm=undefined;
+    cartaDois=undefined;
+}
 
-    function gameOver(){
-        modalGameOver.style.zIndex=10;
-        modalGameOver.addEvenListener("click",startGame,false);
+//confere se o jogo terminou
+function confereSeTerminou(){
+    if(acertos==baralho.length){
+        //chama função que termina o jogo
+        setTimeout(terminar,2650);
+        clearInterval(idCronometro);
+    }else{
+        console.log("continue...");
     }
+}
 
-    function matchCardSing(){
-        imgMatchSign.style.zIndex=1;
-        imgMatchSign.style.top= 150+"px";
-        imgMatchSign.style.opacity=0;
-        setTimeout(function(){
-            imgMatchSign.style.zIndex=-1;
-            imgMatchSign.style.top= 250+"px";
-            imgMatchSign.style.opacity=1;
-        },1500);
+//vira a carta
+function vira(){
+    cartaUm.classList.remove("virada");
+    cartaDois.classList.remove("virada");
+    fixa();
+}
+
+//terminar o jogo
+function terminar(){
+    alert(`Você terminou o jogo em ${movimentos} movimentos e ${tempo}segundos`);
+    const recomeçar=confirm("Deseja jogar novamente?");
+    if(recomeçar==true){
+        window.location.reload();
+    }else{
+        console.log("Obrigado por jogar");
     }
+} 
 
-}())
+//chama a função inicial
+perguntar();
